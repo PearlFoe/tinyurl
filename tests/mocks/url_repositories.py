77647a0a2b4c3b78ca3b6f86749eacb004ac4613
PearlFoe <itknml@gmail.com>
2024-02-09 20:41:25 +0500

    Created cache repository mock

--- tests/mocks/url_repositories.py
+++ tests/mocks/url_repositories.py
@@ -10,3 +10,14 @@ class URLRepositoryMock:
 
     async def get_url(self, short_url_id: URLID) -> URL:
         return self.data.get(short_url_id)
+
+
+class URLCacheRepositoryMock:
+    def __init__(self) -> None:
+        self.data: dict[str, dict[str, URL | float]] = {}
+
+    async def save_url(self, url: URL, ttl: float = -1) -> None:
+        self.data[url.short] = {"value": URL, "ttl": ttl}
+
+    async def get_url(self, short_url_id: URLID) -> URL | None:
+        return self.data.get(short_url_id, {}).get("value")
